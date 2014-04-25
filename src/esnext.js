c09678c7382b0177a132cb74dffed88e82a472bb
shinnn <snnskwtnb@gmail.com>
2014-04-26 05:40:33 +0900

    code refactoring

--- src/esnext.js
+++ src/esnext.js
@@ -20,42 +20,35 @@ var defaultOptions = {
 };
 
 module.exports = function (grunt) {
-  grunt.registerMultiTask(
-    'esnext',
-    'Merge multiple data into a file or Grunt config.',
-    function() {
-      var options = this.options(defaultOptions);
-    
-      // Iterate over all specified src/dest file groups
-      this.files.forEach(function(file) {
-        var src = file.src.filter((filepath) => {
-          if (!grunt.file.exists(filepath)) {
-            grunt.log.warn(`Source file ${ chalk.cyan(filepath) } not found.`);
-            return false;
-          } else {
-            return true;
-          }
-        });
-      
-        if (src.length === 0) {
-          grunt.log.warn(`Destination ${
-            chalk.cyan(f.dest)
-          } not written because src files were empty.`);
-          return;
+  grunt.registerMultiTask('esnext', 'Transpile JS.next to JS.today', function() {
+    var options = this.options(defaultOptions);
+  
+    // Iterate over all specified src/dest file groups
+    this.files.forEach(f => {
+      var src = f.src.filter(filepath => {
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn(`Source file "${ filepath }" not found.`);
+          return false;
+        } else {
+          return true;
         }
-      
-        var code = '';
-      
-        src.forEach((f) => {
-          var result = compile(grunt.file.read(f), options);
-          code += result.code;
-        });
-
-        // Write the destination file if 'dest' is specified
-        grunt.file.write(file.dest, code);
-        // Print a success message
-        grunt.log.writeln(`File ${ chalk.cyan(file.dest) } created.`);
       });
-    }
-  );
+    
+      if (src.length === 0) {
+        grunt.log.warn(`Destination ${
+          chalk.cyan(f.dest)
+        } not written because src files were empty.`);
+        return;
+      }
+    
+      var code = '';
+      src.forEach(filepath => {
+        var result = compile(grunt.file.read(filepath), options);
+        code += result.code;
+      });
+      
+      grunt.file.write(f.dest, code);
+      grunt.log.writeln(`File ${ chalk.cyan(f.dest) } created.`);
+    });
+  });
 };

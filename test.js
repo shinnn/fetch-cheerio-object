bacc12414eb7f39172d6702857a7f31f74bc066e
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2017-11-24 19:46:15 +0900

    update dependnecies

--- test.js
+++ test.js
@@ -1,4 +1,3 @@
-/* eslint-disable newline-per-chained-call */
 'use strict';
 
 const fetchCheerioObject = require('.');
@@ -7,36 +6,46 @@ const test = require('tape');
 test('fetchCheerioObject()', t => {
   t.plan(7);
 
-  t.strictEqual(fetchCheerioObject.name, 'fetchCheerioObject', 'should have a function name.');
+  t.equal(fetchCheerioObject.name, 'fetchCheerioObject', 'should have a function name.');
 
-  fetchCheerioObject('https://example.com/').then($ => {
-    t.strictEqual(
+  fetchCheerioObject('https://example.org/').then($ => {
+    t.equal(
       $('head')
-      .find(':not([charset])').remove().end().html().trim(),
+      .find(':not([charset])')
+      .remove()
+      .end()
+      .html()
+      .trim(),
       '<meta charset="utf-8">',
       'should fetch a HTML and parse it as a cheerio object.'
     );
   }).catch(t.fail);
 
-  fetchCheerioObject('https://example.com/', {xmlMode: true}).then($ => {
-    t.strictEqual(
-      $('head').find(':not([charset])').remove().end().html().trim(),
+  fetchCheerioObject('https://example.org/', {xmlMode: true}).then($ => {
+    t.equal(
+      $('head')
+      .find(':not([charset])')
+      .remove()
+      .end()
+      .html()
+      .trim(),
       '<meta charset="utf-8"/>',
       'should support htmlparser2 options.'
     );
   }).catch(t.fail);
 
   fetchCheerioObject('https://www.npmjs.com/favicon.ico').then($ => {
-    t.strictEqual($('*').html(), null, 'should even read a non-text files.');
+    t.equal($('*').html(), null, 'should even read a non-text files.');
   }).catch(t.fail);
 
-  fetchCheerioObject('https://n/o/t/f.o.u.n.d').then(t.fail, err => {
-    t.ok(/ENOTFOUND.*443/.test(err.message), 'should fail when it cannot get a contents from the URL.');
+  fetchCheerioObject('https://n/o/t/f.o.u.n.d').then(t.fail, ({message}) => {
+    t.ok(/ENOTFOUND.*443/.test(message), 'should fail when it cannot get a contents from the URL.');
   }).catch(t.fail);
 
-  fetchCheerioObject(['https://example.com/']).then(t.fail, err => {
-    t.ok(
-      /TypeError.*must be a string/.test(err),
+  fetchCheerioObject(['https://example.org/']).then(t.fail, ({message}) => {
+    t.equal(
+      message,
+      'The "url" argument must be of type string. Received type object',
       'should fail when it takes a non-string argument as its first argument.'
     );
   }).catch(t.fail);
